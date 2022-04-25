d0ace5b7073e1d01cf2a6aec07ab50dd4ec571e5
Christoph Strobl <cstrobl@vmware.com>
2022-04-25 14:41:36 +0200

    Move entity processing into own post processor that is called from the repository one

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/aot/AotJapRepositoryPostProcessor.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/aot/AotJapRepositoryPostProcessor.java
@@ -15,18 +15,13 @@
  */
 package org.springframework.data.jpa.aot;
 
-import java.lang.reflect.Modifier;
+import java.util.stream.Collectors;
 
 import org.springframework.aot.generator.CodeContribution;
-import org.springframework.aot.hint.MemberCategory;
-import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.data.aot.AotContributingRepositoryBeanPostProcessor;
 import org.springframework.data.aot.AotRepositoryContext;
-import org.springframework.data.aot.RepositoryBeanContribution;
 import org.springframework.data.aot.TypeUtils;
 import org.springframework.util.ClassUtils;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.ReflectionUtils;
 
 /**
  * @author Christoph Strobl
@@ -35,64 +30,18 @@ import org.springframework.util.ReflectionUtils;
 public class AotJapRepositoryPostProcessor extends AotContributingRepositoryBeanPostProcessor {
 
 	@Override
-	public RepositoryBeanContribution contribute(RootBeanDefinition beanDefinition, Class<?> beanType, String beanName) {
-		return super.contribute(beanDefinition, beanType, beanName);
-	}
-
-	@Override
 	protected void contribute(AotRepositoryContext ctx, CodeContribution contribution) {
+
 		super.contribute(ctx, contribution);
 
-		ctx.getResolvedTypes()
-				.stream()
-				.filter(it -> !isJavaOrPrimitiveType(it))
-				.forEach(type -> {
-
-					if (!ObjectUtils.isEmpty(type.getDeclaredClasses())) {
-						contribution.runtimeHints().reflection()
-								.registerType(type, hint -> hint.withMembers(MemberCategory.DECLARED_CLASSES));
-					}
-
-					ReflectionUtils.doWithFields(type, field -> {
-						contribution.runtimeHints().reflection().registerField(field, hint -> hint.allowWrite(true).allowUnsafeAccess(true));
-					}, field -> Modifier.isFinal(field.getModifiers()));
-
-					if (ctx.resolveType("org.hibernate.Hibernate").isPresent()) { // only for you hibernate - xoxo
-						ReflectionUtils.doWithFields(type, field -> {
-							ctx.resolveType("org.hibernate.type.EnumType").ifPresent(it -> {
-								contribution.runtimeHints().reflection().registerType(it, hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
-							});
-						}, field -> field.getType().isEnum());
-					}
-				});
-
-		ctx.getResolvedAnnotations()
-				.stream()
-				.filter(it -> {
-					return TypeUtils.isAnnotationFromOrMetaAnnotated(it.getType(), "jakarta.persistence") || TypeUtils.isAnnotationFromOrMetaAnnotated(it.getType(), "org.hibernate");
-				})
-				.forEach(it -> {
-					contribution.runtimeHints().reflection().registerType(it.getType(), hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS));
-					if (it.isMetaPresent()) {
-						// TODO: do we need extra handling here
-					}
-
-					it.asMap().entrySet().forEach(entry -> {
-						if (entry.getValue() instanceof Class) {
-							Class<?> attributeValue = (Class<?>) entry.getValue();
-							if (!isJavaOrPrimitiveType(attributeValue)) {
-								contribution.runtimeHints().reflection().registerType(attributeValue, hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS));
-							}
-						} else if (entry.getValue() instanceof Class[]) {
-							for (Class<?> attributeValue : (Class<?>[]) entry.getValue()) {
-								if (!isJavaOrPrimitiveType(attributeValue)) {
-									contribution.runtimeHints().reflection().registerType(attributeValue, hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS));
-								}
-							}
-						}
-					});
-				});
+		AotJpaEntityPostProcessor aotJpaEntityPostProcessor = new AotJpaEntityPostProcessor();
+		aotJpaEntityPostProcessor.setBasePackages(ctx.getBasePackages()); // lookup entities in base packages
+		aotJpaEntityPostProcessor.setManagedTypes(ctx.getResolvedTypes().stream().filter(it -> !isJavaOrPrimitiveType(it)).collect(Collectors.toSet())); // add the entities referenced from the repository
+		aotJpaEntityPostProcessor.setBeanFactory(ctx.getBeanFactory()); // the context
+		aotJpaEntityPostProcessor.contributeManagedTypes(ctx.getBeanFactory())
+				.applyTo(contribution); // don't forget to apply stuff
 	}
+
 
 	private static boolean isJavaOrPrimitiveType(Class<?> type) {
 		if (TypeUtils.type(type).isPartOf("java") || type.isPrimitive() || ClassUtils.isPrimitiveArray(type)) {
--- spring-data-jpa/src/main/java/org/springframework/data/jpa/aot/AotJpaEntityPostProcessor.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/aot/AotJpaEntityPostProcessor.java
@@ -15,19 +15,35 @@
  */
 package org.springframework.data.jpa.aot;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.aot.generator.CodeContribution;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.generator.AotContributingBeanPostProcessor;
 import org.springframework.beans.factory.generator.BeanInstantiationContribution;
 import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.data.aot.AotContributingRepositoryBeanPostProcessor;
+import org.springframework.core.annotation.MergedAnnotation;
+import org.springframework.data.aot.AotContext;
+import org.springframework.data.aot.TypeCollector;
+import org.springframework.data.aot.TypeScanner;
+import org.springframework.data.aot.TypeUtils;
+import org.springframework.data.util.Lazy;
 import org.springframework.lang.Nullable;
-import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
-import org.springframework.orm.jpa.SharedEntityManagerCreator;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * @author Christoph Strobl
@@ -35,24 +51,66 @@ import org.springframework.util.ClassUtils;
  */
 public class AotJpaEntityPostProcessor implements AotContributingBeanPostProcessor, BeanFactoryAware {
 
+	private static final Collection<JpaImplementation> JPA_IMPLEMENTATIONS = Arrays.asList(new HibernateJpaImplementation());
+
 	@Nullable
 	private BeanFactory beanFactory;
 
 	@Nullable
 	private Collection<String> packageNames;
 
+	private Set<String> processedPackageNames = new LinkedHashSet<>();
+
+	private Set<Class<?>> managedTypes = Collections.emptySet();
+
+
 	@Override
 	public BeanInstantiationContribution contribute(RootBeanDefinition beanDefinition, Class<?> beanType, String beanName) {
 
-
-		if(!ClassUtils.isAssignable(AbstractEntityManagerFactoryBean.class, beanType)) {
-			return null;
+		if (processedPackageNames.isEmpty() && !CollectionUtils.isEmpty(packageNames) && beanFactory instanceof ConfigurableListableBeanFactory bf) {
+			return contributeManagedTypes(bf);
 		}
 
-		System.out.println("beanDefinition: " + beanDefinition);
-
-
 		return null;
+	}
+
+	ManagedEntitiesContribution contributeManagedTypes(ConfigurableListableBeanFactory bf) {
+
+		Set<Class<?>> types = new LinkedHashSet<>(managedTypes);
+		types.addAll(lookupRelevantTypes(bf.getBeanClassLoader(), packageNames));
+
+		return new ManagedEntitiesContribution(types, new AotContext() {
+			@Override
+			public ConfigurableListableBeanFactory getBeanFactory() {
+				return bf;
+			}
+		});
+	}
+
+
+	Set<Class<?>> lookupRelevantTypes(ClassLoader classLoader, Collection<String> packageNames) {
+
+		processedPackageNames.addAll(packageNames);
+
+		return new TypeScanner(classLoader)
+				.scanForTypesAnnotatedWith(jakarta.persistence.Entity.class, jakarta.persistence.MappedSuperclass.class, jakarta.persistence.Embeddable.class)
+				.scanPackages(packageNames)
+				.stream()
+				.flatMap(type -> {
+					return TypeCollector.inspect(type).list().stream();
+				})
+				.filter(it -> !isJavaOrPrimitiveType(it))
+				.collect(Collectors.toSet())
+				;
+	}
+
+
+	public void setBasePackages(Set<String> basePackages) {
+		packageNames = basePackages;
+	}
+
+	public void setManagedTypes(@Nullable Set<Class<?>> managedTypes) {
+		this.managedTypes = new LinkedHashSet<>(managedTypes);
 	}
 
 	@Override
@@ -63,5 +121,141 @@ public class AotJpaEntityPostProcessor implements AotContributingBeanPostProcess
 	@Override
 	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
 		this.beanFactory = beanFactory;
+	}
+
+	private static boolean isJavaOrPrimitiveType(Class<?> type) {
+		if (TypeUtils.type(type).isPartOf("java") || type.isPrimitive() || ClassUtils.isPrimitiveArray(type)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static Collection<JpaImplementation> availableJpaImplementation(ClassLoader classLoader) {
+		return JPA_IMPLEMENTATIONS.stream().filter(it -> it.isAvailable(classLoader)).collect(Collectors.toSet());
+	}
+
+	static class ManagedEntitiesContribution implements BeanInstantiationContribution {
+
+		AotContext ctx;
+		Set<Class<?>> mangedTypes;
+
+		public ManagedEntitiesContribution(Set<Class<?>> mangedTypes, AotContext ctx) {
+
+			this.ctx = ctx;
+			this.mangedTypes = new LinkedHashSet<>(mangedTypes);
+		}
+
+		@Override
+		public void applyTo(CodeContribution contribution) {
+			mangedTypes.forEach(it -> processManagedType(it, contribution, ctx));
+		}
+	}
+
+	static void processManagedType(Class<?> type, CodeContribution contribution, AotContext ctx) {
+
+		if (type.isAnnotation()) {
+			contribution.runtimeHints().reflection().registerType(type, hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS));
+		} else {
+			contribution.runtimeHints().reflection().registerType(type, hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS, MemberCategory.DECLARED_FIELDS));
+
+			ReflectionUtils.doWithFields(type, field -> {
+				contribution.runtimeHints().reflection().registerField(field, hint -> hint.allowWrite(true).allowUnsafeAccess(true));
+			}, field -> Modifier.isFinal(field.getModifiers()));
+		}
+
+		if (!ObjectUtils.isEmpty(type.getDeclaredClasses())) { // TODO: should we have this in commons?
+			contribution.runtimeHints().reflection()
+					.registerType(type, hint -> hint.withMembers(MemberCategory.DECLARED_CLASSES));
+		}
+
+		invokeJpaImplementationSpecificTreatment(type, contribution, ctx);
+
+		TypeUtils.resolveUsedAnnotations(type)
+				.forEach(it -> {
+					if (TypeUtils.isAnnotationFromOrMetaAnnotated(it.getType(), "jakarta.persistence")) {
+						processPersistenceAnnotation(it, contribution, ctx);
+					}
+					invokeJpaImplementationSpecificTreatment(it, contribution, ctx);
+				});
+	}
+
+	static void processPersistenceAnnotation(MergedAnnotation<Annotation> annotation, CodeContribution contribution, AotContext ctx) {
+
+		contribution.runtimeHints().reflection().registerType(annotation.getType(), hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS));
+		if (annotation.isMetaPresent()) {
+			// TODO: do we need extra handling here
+		}
+
+		annotation.asMap().entrySet().forEach(entry -> {
+			if (entry.getValue() instanceof Class) {
+				Class<?> attributeValue = (Class<?>) entry.getValue();
+				if (!isJavaOrPrimitiveType(attributeValue)) {
+					processManagedType(attributeValue, contribution, ctx);
+				}
+			} else if (entry.getValue() instanceof Class[]) {
+				for (Class<?> attributeValue : (Class<?>[]) entry.getValue()) {
+					if (!isJavaOrPrimitiveType(attributeValue)) {
+						processManagedType(attributeValue, contribution, ctx);
+					}
+				}
+			}
+		});
+	}
+
+	static void invokeJpaImplementationSpecificTreatment(Class<?> type, CodeContribution contribution, AotContext ctx) {
+		availableJpaImplementation(ctx.getClassLoader()).stream()
+				.forEach(it -> it.processManagedType(type, contribution, ctx));
+	}
+
+	static void invokeJpaImplementationSpecificTreatment(MergedAnnotation<Annotation> annotation, CodeContribution contribution, AotContext ctx) {
+		availableJpaImplementation(ctx.getClassLoader()).stream()
+				.filter(it -> TypeUtils.isAnnotationFromOrMetaAnnotated(annotation.getType(), it.getNamespace()))
+				.forEach(it -> it.processPersistenceAnnotation(annotation, contribution, ctx));
+	}
+
+	private interface JpaImplementation {
+
+		String getNamespace();
+
+		boolean isAvailable(ClassLoader classLoader);
+
+		void processManagedType(Class<?> type, CodeContribution contribution, AotContext ctx);
+
+		void processPersistenceAnnotation(MergedAnnotation<Annotation> annotation, CodeContribution contribution, AotContext ctx);
+	}
+
+	private static class HibernateJpaImplementation implements JpaImplementation {
+
+		Lazy<Boolean> available = null;
+
+
+		@Override
+		public void processManagedType(Class<?> type, CodeContribution contribution, AotContext ctx) {
+
+			ReflectionUtils.doWithFields(type, field -> {
+				ctx.resolveType("org.hibernate.type.EnumType").ifPresent(it -> {
+					contribution.runtimeHints().reflection().registerType(it, hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+				});
+			}, field -> field.getType().isEnum());
+		}
+
+		@Override
+		public void processPersistenceAnnotation(MergedAnnotation<Annotation> annotation, CodeContribution contribution, AotContext ctx) {
+			AotJpaEntityPostProcessor.processPersistenceAnnotation(annotation, contribution, ctx);
+		}
+
+		@Override
+		public String getNamespace() {
+			return "org.hibernate";
+		}
+
+		@Override
+		public boolean isAvailable(ClassLoader classLoader) {
+
+			if (available == null) {
+				available = Lazy.of(() -> ClassUtils.isPresent("org.hibernate.Hibernate", classLoader));
+			}
+			return available.get();
+		}
 	}
 }
